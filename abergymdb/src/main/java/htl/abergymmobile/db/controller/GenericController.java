f0d34ec99d4f4d25a2ae3cb128af1434c99bccf2
ValentinDuerk <55828091+ValentinDuerk@users.noreply.github.com>
2022-11-21 11:03:02 +0100

    Update Database GenericController

---
+++ abergymdb/src/main/java/htl/abergymmobile/db/controller/GenericController.java
@@ -0,0 +1,56 @@
+package htl.abergymmobile.db.controller;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@ApplicationScoped
+public class GenericController {
+    /**
+     * The EntityManager
+     */
+    @Inject
+    protected EntityManager em;
+
+    /**
+     * A generic method to add an entity
+     * @param entity a generic entity to insert into the database
+     * @param <T> the generic type T
+     */
+    @Transactional
+    public<T> void add(T entity){
+        if(entity == null) throw new IllegalArgumentException();
+        em.persist(entity);
+    }
+    /**
+     * A generic method to delete an entity
+     * @param entity a generic entity to delete from the database
+     * @param <T> the generic type T
+     */
+    @Transactional
+    public<T> void delete(T entity){
+        if(entity == null) throw new IllegalArgumentException();
+        em.remove(em.contains(entity) ? entity : em.merge(entity));
+
+    }
+    /**
+     * A generic method to update an entity
+     * @param entity a generic entity to update from the database
+     * @param <T> the generic type T
+     */
+    @Transactional
+    public<T> void update(T entity){
+        if(entity == null) throw new IllegalArgumentException();
+        em.merge(entity);
+    }
+    /**
+     * A method that executes all pending statements now instead of later
+     */
+    @Transactional
+    public void flushAndClear() {
+        em.flush();
+        em.clear();
+    }
+}
