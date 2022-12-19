9c6059517db6760751e6e410f211bd1b9f27868e
SonToni <antoniokuvac35@gmail.com>
2022-12-19 21:15:10 +0100

    created shadowdom.js
    created shadowdom.js
    deleted iframes

---
+++ Frontend/scripts/shadowdom.js
@@ -0,0 +1,26 @@
+const tabPanel1 = document.querySelector('#tabpanel-1');
+const shadow1 = tabPanel1.attachShadow({mode: 'open'});
+
+fetch('html/workoutPlanCreation.html')
+  .then(response => response.text())
+  .then(html => {
+    shadow1.innerHTML = html;
+  });
+
+  const tabPanel2 = document.querySelector('#tabpanel-2');
+const shadow2 = tabPanel2.attachShadow({mode: 'open'});
+
+fetch('html/templates.html')
+  .then(response => response.text())
+  .then(html => {
+    shadow2.innerHTML = html;
+  });
+
+  const tabPanel3 = document.querySelector('#tabpanel-3');
+const shadow3 = tabPanel3.attachShadow({mode: 'open'});
+
+fetch('html/user.html')
+  .then(response => response.text())
+  .then(html => {
+    shadow3.innerHTML = html;
+  });
